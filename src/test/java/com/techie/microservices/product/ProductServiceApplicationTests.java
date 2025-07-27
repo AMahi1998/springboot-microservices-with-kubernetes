062fd7b4f2700430293e35ad1a17115dab4cd853
AMahi1998 <amahima888@gmail.com>
2025-07-27 00:31:52 -0400

    Added Product Service Integration Tests

--- src/test/java/com/techie/microservices/product/ProductServiceApplicationTests.java
+++ src/test/java/com/techie/microservices/product/ProductServiceApplicationTests.java
@@ -1,15 +1,46 @@
 package com.techie.microservices.product;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@LocalServerPort
+	private Integer port;
+	@BeforeEach
+	void setup(){
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
 	}
 
+	@Test
+	void shouldCreateProduct() {
+		String requestBody = """
+				{
+					"name": "iPhone 14",
+					"description": "Apple iPhone 14",
+					"price": 1200
+				}
+				""";
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo("iPhone 14"))
+				.body("description", Matchers.equalTo("Apple iPhone 14"))
+				.body("price", Matchers.equalTo(1200.00F));
+
+
+	}
 }
