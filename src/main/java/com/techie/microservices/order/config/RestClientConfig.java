730ac23f82819f48a78951cab52e2e522a556e48
AMahi1998 <amahima888@gmail.com>
2025-08-06 00:43:34 -0400

    Updated docker-compose to include Apache Kafka services

--- src/main/java/com/techie/microservices/order/config/RestClientConfig.java
+++ src/main/java/com/techie/microservices/order/config/RestClientConfig.java
@@ -2,11 +2,9 @@ package com.techie.microservices.order.config;
 
 import com.techie.microservices.order.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
-import org.springframework.boot.web.client.ClientHttpRequestFactories;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -22,17 +20,23 @@ public class RestClientConfig {
     public InventoryClient inventoryClient(){
         RestClient restClient=RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
-                .requestFactory(getClientRequestFactory())
+                //.requestFactory(getClientRequestFactory())
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
 
     }
-    private ClientHttpRequestFactory getClientRequestFactory(){
-        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings= ClientHttpRequestFactorySettings.DEFAULTS
-                .withConnectTimeout(Duration.ofSeconds(3))
-                .withReadTimeout(Duration.ofSeconds(3));
-        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
-    }
+   // private ClientHttpRequestFactory getClientRequestFactory(){
+
+//        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+//        factory.setConnectTimeout(Duration.ofSeconds(3));
+//        factory.setReadTimeout(Duration.ofSeconds(3));
+//        return factory;
+//        // Alternative way to create ClientHttpRequestFactory using ClientHttpRequestFactories
+//        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings= ClientHttpRequestFactorySettings.DEFAULTS
+//                .withConnectTimeout(Duration.ofSeconds(3))
+//                .withReadTimeout(Duration.ofSeconds(3));
+//        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
+//    }
 }
