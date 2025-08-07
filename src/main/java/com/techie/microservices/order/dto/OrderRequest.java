5a1ede852278a90661cc450de08cfa521289b875
AMahi1998 <amahima888@gmail.com>
2025-08-06 20:17:41 -0400

    Sent message to Kafka topic using KafkaTemplate

--- src/main/java/com/techie/microservices/order/dto/OrderRequest.java
+++ src/main/java/com/techie/microservices/order/dto/OrderRequest.java
@@ -2,5 +2,8 @@ package com.techie.microservices.order.dto;
 
 import java.math.BigDecimal;
 
-public record OrderRequest(Long id, String orderNumber, String skuCode, BigDecimal price, Integer quantity) {
+public record OrderRequest(Long id, String orderNumber, String skuCode, BigDecimal price, Integer quantity, UserDetails userDetails) {
+    // This record class is used to represent the request format for creating an order.
+    // It includes fields for id, orderNumber, skuCode, price, quantity, and userDetails.
+    public record UserDetails(String email, String firstName, String lastName){}
 }
