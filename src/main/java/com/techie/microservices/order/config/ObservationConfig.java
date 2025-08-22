8ca36a9c33e41232623386721b73801fe0452b8a
AMahi1998 <amahima888@gmail.com>
2025-08-22 00:34:04 -0400

    Resolved Tempo Bug by including Kafka producer config

--- src/main/java/com/techie/microservices/order/config/ObservationConfig.java
+++ src/main/java/com/techie/microservices/order/config/ObservationConfig.java
@@ -2,9 +2,21 @@ package com.techie.microservices.order.config;
 
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.aop.ObservedAspect;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
 
+@Configuration(proxyBeanMethods = false)
+@RequiredArgsConstructor
 public class ObservationConfig {
+    private final KafkaTemplate kafkaTemplate;
+
+    @PostConstruct
+    void setup() {
+        this.kafkaTemplate.setObservationEnabled(true);
+    }
     @Bean
     ObservedAspect observedAspect(ObservationRegistry registry) {
         return new ObservedAspect(registry);
